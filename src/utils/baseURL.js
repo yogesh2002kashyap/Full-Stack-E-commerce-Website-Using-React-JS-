bce8b68fe602f43c0d25a340422778a0b8d5b00d
yogesh2002kashyap <yogesh2003kashyap@gmail.com>
2025-11-13 14:13:22 +0530

    fixed the file structure

--- src/utils/baseURL.js
+++
@@ -1,5 +0,0 @@
-const getBaseURL = () => {
-    return "http://localhost:5000"
-}
-
-export default getBaseURL;
--- src/utils/getImgUrl.js
+++
@@ -1,6 +0,0 @@
-function getImgUrl(name){
-    return new URL(`../assets/books/${name}` , import.meta.url)
-
-}
-
-export {getImgUrl}
